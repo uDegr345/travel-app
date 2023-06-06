4fcfaceb279274c6f8889ca72ad3b42cf19a2af3
unknown <uDegr@proton.me>
2023-06-07 01:45:08 +0200

    changed api call implementation fixed route bug and moved fetchurl building to client class

--- src/client/js/weatherbit.js
+++ src/client/js/weatherbit.js
@@ -1,20 +1,28 @@
 
 
 const getWeatherData =  async(lat, lon) => {
-    const baseUrl = "https://api.weatherbit.io/v2.0/forecast/daily" 
+    const baseUrl = "https://api.weatherbit.io/v2.0/forecast/daily?" 
     
 
-    const fetchUrl = `${baseUrl}&lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`
+    const fetchUrl = `${baseUrl}lat=${lat}&lon=${lon}&key=${process.env.WEATHERBIT_API_KEY}`
 
-    const response = await fetch('/geolocationCall', {
+    const response = await fetch('/weatherbitCall', {
         method: 'POST',
         credentials: 'same-origin',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ "lat": lat, "lon": lon, "url" : baseUrl})
+        body: JSON.stringify({"weatherbitUrl" : fetchUrl})
     })
+    const res = await response.json()
+    console.log(res)
+
+    const temp = res.data[0].temp
+    const cityName = res.city_name
+
+    return [temp,cityName]
+
 
 
 }
