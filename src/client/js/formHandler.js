75b5cfb73aa75cd143e4196c2538ec9434a22428
unknown <uDegr@proton.me>
2023-06-08 00:51:12 +0200

    ui function expanded and else added

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,19 @@
 async function handleNewTravel(event) {
-    event.preventDefault();
+    const tripDateElement = document.getElementById('tripDate')
+    const tripDateValue = tripDateElement.value
+    if(Client.isDateInRange(tripDateValue) ==  true) {
+        event.preventDefault();
 
-    const [lat, lon] = await Client.getGeolocation()
-    const [temp, cityName] = await Client.getWeatherData(lat, lon)
-    const imageLink = await Client.getLocationImage(cityName)
+        const [lat, lon] = await Client.getGeolocation()
+        const [temp, cityName] = await Client.getWeatherData(lat, lon)
+        const imageLink = await Client.getLocationImage(cityName)
 
-    Client.updateTripGrid(imageLink, cityName, temp)
+        Client.updateTripGrid(imageLink, cityName, tripDateValue, temp)
 
 
-
+    } else {
+        alert('date is too far in the future (max 16 days)')
+    }
 }
 
 export {
--- src/client/js/uiHandler.js
+++ src/client/js/uiHandler.js
@@ -1,10 +1,8 @@
-function updateTripGrid(imageLink, cityName, temp) {
-createTripCard(imageLink, cityName, temp)
-
-
+function updateTripGrid(imageLink, cityName, tripDateValue, temp) {
+    createTripCard(imageLink, cityName, tripDateValue, temp)
 }
 
-function createTripCard(imageLink, cityName, temp) {
+function createTripCard(imageLink, cityName, tripDateValue, temp) {
     const section =  document.getElementById("tripGrid")
     const fragment = new DocumentFragment();
 
@@ -12,7 +10,7 @@ function createTripCard(imageLink, cityName, temp) {
     const article = document.createElement('article')
     article.classList.add('tripCard')
 
-    
+    //crate the html for the image
     const tripImgContainer =  document.createElement('p')
     
     const image = document.createElement('img')
@@ -20,17 +18,28 @@ function createTripCard(imageLink, cityName, temp) {
     image.setAttribute('alt' , `a picture of ${cityName}`)
     image.classList.add('tripImg')
 
+    //create the html for the city name
     const  cityNameElement =  document.createElement('p')
     cityNameElement.innerText = cityName
     cityNameElement.classList.add('name')
 
+    //create the html for the travel date
+    const dateElement = document.createElement('p')
+    const tripDate = new Date(tripDateValue)
+    const daysToTravel = Client.calculateDaysToTravel(tripDate)
+    dateElement.innerText = ` The trip is in ${daysToTravel} days`
+    dateElement.classList.add('tripDate')
+
+    //create the html for the temperature
     const tempElement = document.createElement('temp')
-    tempElement.innerText = temp
+    tempElement.innerText = `The temperature in your destination will be approximately ${temp} \u2103`
     tempElement.classList.add('tripTemp');
 
+    //append all created elements
     tripImgContainer.appendChild(image)
     article.appendChild(tripImgContainer)
     article.appendChild(cityNameElement)
+    article.appendChild(dateElement)
     article.appendChild(tempElement)
 
     fragment.appendChild(article)
